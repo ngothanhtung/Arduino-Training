543cfa9f2acb576a8733b04018b0389ad6c7714c
Ngo Thanh Tung <tungnt@softech.vn>
2019-09-05 01:15:56 +0700

    Update Nhiet Do + Do Am

--- build/libraries/dht11/DHT.cpp.d
+++ build/libraries/dht11/DHT.cpp.d
@@ -1,5 +1,6 @@
-c:\Working\Github\Arduino-Training\build\sketch\liquid_display_sample.ino.cpp.o: \
- c:\Working\Github\Arduino-Training\build\sketch\liquid_display_sample.ino.cpp \
+c:\Working\Github\Arduino-Training\build\libraries\dht11\DHT.cpp.o: \
+ C:\Users\Tungn\OneDrive\Documents\Arduino\libraries\dht11\DHT.cpp \
+ C:\Users\Tungn\OneDrive\Documents\Arduino\libraries\dht11\DHT.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
@@ -10,8 +11,4 @@ c:\Working\Github\Arduino-Training\build\sketch\liquid_display_sample.ino.cpp.o:
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Users\Tungn\OneDrive\Documents\Arduino\libraries\LiquidCrystal_I2C/LiquidCrystal_I2C.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
--- dht-sample/dht-sample.ino
+++ dht-sample/dht-sample.ino
@@ -1,13 +1,11 @@
-# 1 "c:\\Working\\Github\\Arduino-Training\\dht-sample\\dht-sample.ino"
-# 1 "c:\\Working\\Github\\Arduino-Training\\dht-sample\\dht-sample.ino"
-# 2 "c:\\Working\\Github\\Arduino-Training\\dht-sample\\dht-sample.ino" 2
-# 3 "c:\\Working\\Github\\Arduino-Training\\dht-sample\\dht-sample.ino" 2
+#include "DHT.h"
+#include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-
+#define DHTPIN 7 // what pin we're connected to
 
 // Uncomment whatever type you're using!
-
+#define DHTTYPE DHT11 // DHT 11
 //#define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
@@ -16,7 +14,7 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 for a 16 char
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-DHT dht(7 /* what pin we're connected to*/, 11 /* DHT 11*/);
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
